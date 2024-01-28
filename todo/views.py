dad9c41c224e23adb7167d03a9f74bc825df47f8
Marcel Herzog <119870192+MarcelDragZ@users.noreply.github.com>
2024-01-28 18:01:15 +0100

    start before

--- todo/views.py
+++ todo/views.py
@@ -8,9 +8,6 @@ from .models import Todo
 from .serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited
-    """
     queryset = Todo.objects.all().order_by('-createdTime')
     serializer_class = TodoSerializer
     permission_classes = [] # permissions.IsAuthenticated # user must be logged in
